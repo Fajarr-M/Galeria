82a3d405501d8e0f0597b1195fe8b9e3568119c3
Fajar M <habibimochfajar.ah@gmail.com>
2021-04-27 14:51:40 +0700

    update email

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,13 @@
+<tr>
+<td>
+<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td class="content-cell" align="center">
+{{ Illuminate\Mail\Markdown::parse($slot) }}
+<a href="instagram.com/fmhabil._"><img src="https://www.transparentpng.com/thumb/logo-instagram/z75gfy-instagram-logo-png.png" alt="instagram logo png @transparentpng.com" style="width: 30px;"></a>
+<a href="twitter.com/mlkywayy_"><img src="https://www.transparentpng.com/thumb/twitter/twitter-bird-logo-pictures-0.png" alt="twitter bird logo pictures @transparentpng.com" style="width: 30px;"></a>
+</td>
+</tr>
+</table>
+</td>
+</tr>
