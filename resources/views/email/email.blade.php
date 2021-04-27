780b3e3acdcfa1d6f1fce94e37ddf90ccc255c91
Fajar M <habibimochfajar.ah@gmail.com>
2021-04-27 15:24:25 +0700

    update layout email

--- resources/views/email/email.blade.php
+++ resources/views/email/email.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Galeria
 
-Hi, Sir   
+Hi, Sir <br>
 Thanks For Joining To {{ config('app.name') }}  
 
 @component('mail::button', ['url' => 'http://galeria.test'])
@@ -10,4 +10,10 @@ Galeria
 
 Thanks,<br>
 {{ config('app.name') }}
+<br>
+<div class="section text-center fs-1">© {{ date('Y') }} {{ config('app.name') }}. All Rights Reseved
+<br>
+<div class="sosmed">
+<a href="instagram.com/fmhabil._"><img src="https://www.transparentpng.com/thumb/logo-instagram/z75gfy-instagram-logo-png.png" alt="instagram logo png @transparentpng.com" style="width: 30px; margin-right:10px;"></a>
+<a href="twitter.com/mlkywayy_"><img src="https://www.transparentpng.com/thumb/twitter/twitter-bird-logo-pictures-0.png" alt="twitter bird logo pictures @transparentpng.com" style="width: 30px; margin-right:10px;"></a></div></div>
 @endcomponent
