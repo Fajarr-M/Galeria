878010dd96340e7b0529d9b85a518c785df96256
Fajar M <habibimochfajar.ah@gmail.com>
2021-04-29 13:02:30 +0700

    update debug

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
+@if (trim($slot) === 'Galeria')
+<div class="logo" style="font-size: 40px; font-weight: bold;">Galeria</div>
 @else
 {{ $slot }}
 @endif
